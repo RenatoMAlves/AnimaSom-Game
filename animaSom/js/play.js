42571f91897bc21f4a9c32183dc43c14a2a01669
Renato Marinho <cmp.1b.renato@gmail.com>
2017-06-06 20:52:54 -0300

    Alterações na fase-cidade. Implementação do contador

--- animaSom/js/play.js
+++ animaSom/js/play.js
@@ -45,36 +45,90 @@ var playState = {
         this.passaro.width = 300;
         this.passaro.height = 170;
         
-        this.statusAcerto=false;
-        this.contador=0;
+        // TEXTO DE SUCESSO
 
-        game.time.events.add(Phaser.Timer.SECOND * 2, this.start, this);
+        this.text = null;
+        this.textReflect = null;
+
+        text = game.add.text(game.world.centerX, game.world.centerY -250, "- PARABÉNS -");
+        text.visible = false;
+
+
+        //  Centers the text
+        text.anchor.set(0.5);
+        text.align = 'center';
+
+        //  Our font + size
+        text.font = 'Arial';
+        text.fontWeight = 'bold';
+        text.fontSize = 70;
+        text.fill = '#2bd800';
+
+        //  Here we create our fake reflection :)
+        //  It's just another Text object, with an alpha gradient and flipped vertically
+
+        textReflect = game.add.text(game.world.centerX, game.world.centerY -200, "- PARABÉNS -");
+
+        //  Centers the text
+        textReflect.anchor.set(0.5);
+        textReflect.align = 'center';
+        textReflect.scale.y = -1;
+
+        //  Our font + size
+        textReflect.font = 'Arial';
+        textReflect.fontWeight = 'bold';
+        textReflect.fontSize = 70;
+        textReflect.fill = '#70ff4c';
+
+        //  Here we create a linear gradient on the Text context.
+        //  This uses the exact same method of creating a gradient as you do on a normal Canvas context.
+        var grd = textReflect.context.createLinearGradient(0, 0, 0, text.canvas.height);
+
+        //  Add in 2 color stops
+        grd.addColorStop(0, 'rgba(255,255,255,0)');
+        grd.addColorStop(1, 'rgba(112, 255, 76,0.5)');
+
+        //  And apply to the Text
+        textReflect.fill = grd;
+
+        textReflect.visible = false;
+        text.visible = false;
+
+        // FIM DO TEXTO DE SUCESSO
+        this.tempo = 90;
+
+        this.time = game.add.text(970, 16, 'Tempo Restante: '+ parseInt(this.tempo),{fontSize: '25px', fill:"#f02"})
+
+        game.time.events.add(1000, this.start, this);
+
+        this.iniciarTempo = false;
 
     },
 
     start: function (){    
         this.ocultarOpcoes(1);
-        this.groupCidade.children[0].x += 230; 
-        this.cachorro.x += 230;
-        this.groupCidade.children[0].y -= 70; 
-        this.cachorro.y -= 70;
+        this.groupCidade.children[0].x = 350; 
+        this.cachorro.x = 360;
+        this.groupCidade.children[0].y = 70; 
+        this.cachorro.y = 80;
         game.add.tween(this.groupCidade.children[0].scale).to( { x: 2, y: 2 }, 3000, Phaser.Easing.Elastic.Out, true);
         game.add.tween(this.cachorro.scale).to( { x: 1, y: 1 }, 3000, Phaser.Easing.Elastic.Out, true);
 
-        game.time.events.add(Phaser.Timer.SECOND * 5, this.initial, this);
+        game.time.events.add(5000, this.initial, this);
     },
 
     initial: function (){
         game.add.tween(this.groupCidade.children[0].scale).to( { x: 1, y: 1 }, 1000, Phaser.Easing.Linear.In, true);
         game.add.tween(this.cachorro.scale).to( { x: 0.5, y: 0.5 }, 1000, Phaser.Easing.Linear.In, true);
-        this.groupCidade.children[0].x -= 230; 
-        this.cachorro.x -= 230;
-        this.groupCidade.children[0].y += 70; 
-        this.cachorro.y += 70;
+        this.groupCidade.children[0].x = 100; 
+        this.cachorro.x = 110;
+        this.groupCidade.children[0].y = 200; 
+        this.cachorro.y = 210;
         this.background.alpha = 0.5;
 
         this.apresentarOpcoes();
 
+        this.iniciarTempo = true;
     },
 
     ocultarOpcoes: function (){
@@ -94,16 +148,24 @@ var playState = {
             game.add.tween(this.groupCidade.children[2]).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
             game.add.tween(this.gato).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
             game.add.tween(this.passaro).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
-            console.log(this.groupCidade);
         }
     },
 
     verificaSelecionado: function(sprite){
         if(sprite.name == 'opcao-0'){
-            console.log('sucesso');
+            textReflect.visible = true;
+            text.visible = true;
+            game.time.events.add(5000, this.Win, this);
+            this.iniciarTempo = false;
         }
-        else
-            console.log('puts');
+        else{
+            textReflect.visible = false;
+            text.visible = false;
+            this.iniciarTempo = false;
+            this.tempo = 91;
+            this.start();
+        }
+            
     },
 
 
@@ -111,6 +173,11 @@ var playState = {
 
         game.time.events.add(Phaser.Timer.SECOND * 7, activateBotoes, this);
 
+        if(this.iniciarTempo){
+            this.tempo = this.tempo - Phaser.Timer.SECOND * 1.9/100000;
+            this.time.text = "Tempo Restante: "+ parseInt(this.tempo);
+        }
+    
         function activateBotoes(){
             this.groupCidade.onChildInputDown.add(onDown, this);
             this.groupCidade.onChildInputOver.add(onOver, this);
@@ -134,6 +201,6 @@ var playState = {
     },
 
     Win: function(){
-        game.state.start('win');
+        game.state.start('menu');
     }
 };
