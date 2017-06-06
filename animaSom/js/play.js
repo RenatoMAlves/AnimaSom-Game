413c0230032ec5203ef1fa814cb844aa63cff7c8
Renato Marinho Alves <cmp.1b.renato@gmail.com>
2017-06-06 18:34:30 -0300

    1ª parte da fase-cidade com a seleção de sprites.

--- animaSom/js/play.js
+++ animaSom/js/play.js
@@ -1,15 +1,15 @@
 var playState = {
 
     create: function(){
-        var background = game.add.sprite(0, 0, 'cidade');
-        background.width = 1300;
-        background.height = 650;
+        this.background = game.add.sprite(0, 0, 'cidade');
+        this.background.width = 1300;
+        this.background.height = 650;
 
         graphics = game.add.graphics(0, 0);
 
-        groupCidade = game.add.group();
+        this.groupCidade = game.add.group();
 
-        groupCidade.inputEnableChildren = true;
+        this.groupCidade.inputEnableChildren = true;
 
         var x = 100;
 
@@ -20,89 +20,107 @@ var playState = {
             graphics.drawRoundedRect(x, 200, 315, 190, 10);
             graphics.endFill();
 
-            var button = groupCidade.create(x, 200, graphics.generateTexture());
+            var button = this.groupCidade.create(x, 200, graphics.generateTexture());
             button.tint = 0xff8800;
-            button.name = 'groupCidade-child-' + i;
+            button.name = 'opcao-' + i;
 
             x = x + 400;
         }
         graphics.destroy();
 
-        var cachorro = game.add.sprite(110, 210, 'cachorro'); 
-        cachorro.width = 300;
-        cachorro.height = 170;
+        this.cachorro = game.add.sprite(110, 210, 'cachorro'); 
+        this.cachorro.width = 300;
+        this.cachorro.height = 170;
         
 
         // Desenha o gato e a borda da box
 
-        var gato = game.add.sprite(510, 210, 'gato'); 
-        gato.width = 300;
-        gato.height = 170;
+        this.gato = game.add.sprite(510, 210, 'gato'); 
+        this.gato.width = 300;
+        this.gato.height = 170;
 
         // Desenha o passaro e a borda da box
 
-        var passaro = game.add.sprite(910, 210, 'passaro'); 
-        passaro.width = 300;
-        passaro.height = 170;
+        this.passaro = game.add.sprite(910, 210, 'passaro'); 
+        this.passaro.width = 300;
+        this.passaro.height = 170;
+        
+        this.statusAcerto=false;
+        this.contador=0;
 
-        game.time.events.add(Phaser.Timer.SECOND * 2, start, this);
+        game.time.events.add(Phaser.Timer.SECOND * 2, this.start, this);
 
-        function start(){
+    },
+
+    start: function (){    
+        this.ocultarOpcoes(1);
+        this.groupCidade.children[0].x += 230; 
+        this.cachorro.x += 230;
+        this.groupCidade.children[0].y -= 70; 
+        this.cachorro.y -= 70;
+        game.add.tween(this.groupCidade.children[0].scale).to( { x: 2, y: 2 }, 3000, Phaser.Easing.Elastic.Out, true);
+        game.add.tween(this.cachorro.scale).to( { x: 1, y: 1 }, 3000, Phaser.Easing.Elastic.Out, true);
+
+        game.time.events.add(Phaser.Timer.SECOND * 5, this.initial, this);
+    },
+
+    initial: function (){
+        game.add.tween(this.groupCidade.children[0].scale).to( { x: 1, y: 1 }, 1000, Phaser.Easing.Linear.In, true);
+        game.add.tween(this.cachorro.scale).to( { x: 0.5, y: 0.5 }, 1000, Phaser.Easing.Linear.In, true);
+        this.groupCidade.children[0].x -= 230; 
+        this.cachorro.x -= 230;
+        this.groupCidade.children[0].y += 70; 
+        this.cachorro.y += 70;
+        this.background.alpha = 0.5;
+
+        this.apresentarOpcoes();
+
+    },
+
+    ocultarOpcoes: function (){
+        this.background.alpha = 0.5;
+        if(1 == 1){
+            this.groupCidade.children[1].alpha = 0;
+            this.groupCidade.children[2].alpha = 0;
+            this.gato.alpha = 0;
+            this.passaro.alpha = 0;
+        }
+    },
+
+    apresentarOpcoes: function(){
+        game.add.tween(this.background).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
+        if(1 == 1){
+            game.add.tween(this.groupCidade.children[1]).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
+            game.add.tween(this.groupCidade.children[2]).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
+            game.add.tween(this.gato).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
+            game.add.tween(this.passaro).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
+            console.log(this.groupCidade);
+        }
+    },
+
+    verificaSelecionado: function(sprite){
+        if(sprite.name == 'opcao-0'){
             
-            ocultarOpcoes(1);
-            groupCidade.children[0].x += 230; 
-            cachorro.x += 230;
-            groupCidade.children[0].y -= 70; 
-            cachorro.y -= 70;
-            game.add.tween(groupCidade.children[0].scale).to( { x: 2, y: 2 }, 3000, Phaser.Easing.Elastic.Out, true);
-            game.add.tween(cachorro.scale).to( { x: 1, y: 1 }, 3000, Phaser.Easing.Elastic.Out, true);
-
-            game.time.events.add(Phaser.Timer.SECOND * 5, initial, this);
         }
+        else
+            console.log('puts');
+    },
 
-        function initial(){
-            game.add.tween(groupCidade.children[0].scale).to( { x: 1, y: 1 }, 1000, Phaser.Easing.Linear.In, true);
-            game.add.tween(cachorro.scale).to( { x: 0.5, y: 0.5 }, 1000, Phaser.Easing.Linear.In, true);
-            groupCidade.children[0].x -= 230; 
-            cachorro.x -= 230;
-            groupCidade.children[0].y += 70; 
-            cachorro.y += 70;
-            background.alpha = 0.5;
 
-            apresentarOpcoes(1);
+    update: function(){
 
-            activateButtons();
-        }
+        game.time.events.add(Phaser.Timer.SECOND * 7, activateBotoes, this);
 
-        function ocultarOpcoes(numero){
-            background.alpha = 0.5;
-            if(numero == 1){
-                groupCidade.children[1].alpha = 0;
-                groupCidade.children[2].alpha = 0;
-                gato.alpha = 0;
-                passaro.alpha = 0;
-            }
-        }
-
-        function apresentarOpcoes(numero){
-            game.add.tween(background).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
-            if(numero == 1){
-                game.add.tween(groupCidade.children[1]).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
-                game.add.tween(groupCidade.children[2]).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
-                game.add.tween(gato).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
-                game.add.tween(passaro).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.In, true);
-            }
-        }
-
-        function activateButtons(){
-            groupCidade.onChildInputDown.add(onDown, this);
-            groupCidade.onChildInputOver.add(onOver, this);
-            groupCidade.onChildInputOut.add(onOut, this);
+        function activateBotoes(){
+            this.groupCidade.onChildInputDown.add(onDown, this);
+            this.groupCidade.onChildInputOver.add(onOver, this);
+            this.groupCidade.onChildInputOut.add(onOut, this);
         }
 
         function onDown (sprite) {
             sprite.tint = 0x00ff00;
-        }
+            this.verificaSelecionado(sprite);
+        }   
 
         function onOver (sprite) {
             sprite.tint = 0xffff00;
@@ -113,12 +131,6 @@ var playState = {
             // sprite.tint = Math.random() * 0xffffff;
 
         }
-
-    },
-
-
-    update: function(){
-        
     },
 
     Win: function(){
